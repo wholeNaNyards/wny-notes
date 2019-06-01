c9eb097f087f2d6acbaa830b06c809f2b4fe4e84
wholeNaNyards <wholenanyards@gmail.com>
2019-05-31 20:39:09 -0700

    Breaking something else on purpose

--- create.js
+++ create.js
@@ -1,9 +1,9 @@
-gibberish.what;
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+  gibberish.what;
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
